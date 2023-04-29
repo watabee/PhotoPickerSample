07478d9535f19ce9333c398dce225d6f2ad979c3
watabee <watabee.dev@gmail.com>
2023-04-29 23:24:52 +0900

    Photo Picker のサンプル実装

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # PhotoPickerSample
 Android の Photo Picker のサンプル
+
+Android OS 10 の端末で試してみたところ、Photo Picker は表示されるが複数画像の選択ができなかった...
+
+## 参考
+
+- https://developer.android.com/training/data-storage/shared/photopicker
+- https://android-developers.googleblog.com/2023/04/photo-picker-everywhere.html
--- app/src/main/java/com/github/watabee/photopickersample/MainActivity.kt
+++ app/src/main/java/com/github/watabee/photopickersample/MainActivity.kt
@@ -1,20 +1,42 @@
 package com.github.watabee.photopickersample
 
+import android.net.Uri
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.result.PickVisualMediaRequest
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.pager.HorizontalPager
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
 import com.github.watabee.photopickersample.ui.theme.PhotoPickerSampleTheme
 
 class MainActivity : ComponentActivity() {
+
+    private val selectedImageUris = mutableStateListOf<Uri>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val pickMedia = registerForActivityResult(ActivityResultContracts.PickMultipleVisualMedia(10)) { uris ->
+            selectedImageUris.clear()
+            selectedImageUris.addAll(uris)
+        }
         setContent {
             PhotoPickerSampleTheme {
                 // A surface container using the 'background' color from the theme
@@ -22,25 +44,59 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    ImageViewer(uris = selectedImageUris) {
+                        pickMedia.launch(PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.ImageOnly))
+                    }
                 }
             }
         }
     }
 }
 
+@OptIn(ExperimentalFoundationApi::class)
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun ImageViewer(uris: List<Uri>, onPickImagesButtonClicked: () -> Unit) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(all = 8.dp),
+        verticalArrangement = Arrangement.spacedBy(8.dp)
+    ) {
+        if (uris.isEmpty()) {
+            Text(
+                modifier = Modifier
+                    .align(Alignment.CenterHorizontally)
+                    .weight(1f),
+                text = "No image selected"
+            )
+        } else {
+            HorizontalPager(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .weight(1f),
+                pageCount = uris.size
+            ) { index ->
+                AsyncImage(
+                    modifier = Modifier.fillMaxSize(),
+                    model = uris[index],
+                    contentScale = ContentScale.Fit,
+                    contentDescription = null
+                )
+            }
+        }
+
+        Button(
+            modifier = Modifier.align(Alignment.CenterHorizontally),
+            onClick = onPickImagesButtonClicked
+        ) {
+            Text(text = "Pick images")
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     PhotoPickerSampleTheme {
-        Greeting("Android")
     }
 }
